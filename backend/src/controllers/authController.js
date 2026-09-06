310794af6562fbc6904ea7cd69aa0428c3dc7b20
faizansiddiqui1 <faizansiddiqui7317@gmail.com>
2026-09-06 10:46:52 +0530

    vrtv667

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -10,7 +10,6 @@ const logger = require('../utils/logger');
 
 const VALID_ROLES = ['super_admin', 'rm', 'member', 'client'];
 
-
 function normalizePhone(input) {
   if (!input) return null;
   let p = String(input).trim().replace(/[\s-()]/g, '');
